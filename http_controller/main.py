b4b7a94b4efaa73e61adc898ec71c6be1a4a6242
Eugene Y. Jen <ejen@amplify.com>
2015-03-26 10:55:16 -0400

    update main.py

--- http_controller/main.py
+++ http_controller/main.py
@@ -8,10 +8,17 @@ def hello():
     msg = """
     <html><head><title>Cheezoid</title></head>
     <body><h3>cheezoid httpd controller</h3>
+    RESTful endpoints
     <ul>
     <li>/status to view cheezoid status</li>
     <li>/cmd to send command</li>
+        <ul>
+        <li>mov (relative angle in degree, relative distance in cm)</li>
+        <li>pen (up/down) </li>
+        <li>reset</li>
+        </ul>
     <li>/svg to send svg</li>
+    <li>/canvas to GUI</li>
     </ul>
     </body>
     </html>
