c47a4f8b03fbd2fe99cc3a5cfd00fb63edfa9823
Eugene Y. Jen <ejen@amplify.com>
2015-03-26 12:36:18 -0400

    initial cheesed reading fifo

--- cheesed/main.py
+++ cheesed/main.py
@@ -1,19 +1,30 @@
 import time
 import sys
 import signal
+import os
 
 RUNNING = True
+PIPENAME = "/tmp/cheese_plate"
 
 def int_signal_handler(signal, frame):
     global RUNNING
     RUNNING = False
 
+def create_fifo(pipename):
+    if not (os.path.isfile(pipename)):
+        os.mkfifo(pipename)
+    fifo = open(pipename, 'r')
+    return fifo
+
+def repl(fifo):
+    print("cheesed repl loop")
 
 def main():
+    fifo = create_fifo(PIPENAME)
     signal.signal(signal.SIGINT, int_signal_handler)
     while(RUNNING):
         time.sleep(1)
-        print('cheesed is running')
+        repl(fifo)
 
 
 if __name__ == '__main__':
