c2d3e2def6aaa302c092caaf4eb81f6fc8a05ded
Milen Pavlov <mpavlov@amplify.com>
2015-03-27 16:49:12 -0400

    rsync only updated; don't rm -rf all

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,7 @@ function log() {
 
 function copy_files() {
   log "Deploying ${DIR} to ${PI_HOSTNAME}..."
-  ssh -n ${SSH_DEST} "rm -rf ~/cheezoid/"
-  rsync -v -r --exclude=".*" --exclude=".*/" "${DIR}" ${SSH_DEST}:.
+  rsync -v --exclude=".*" --exclude=".*/" --update --archive --delete "${DIR}" ${SSH_DEST}:.
 }
 
 function restart_services() {
