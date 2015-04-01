26da5169b19bf0ce4c56ff10d83d6e200223e69e
Ilya Sukhanov <ilya@sukhanov.net>
2015-03-31 23:23:29 -0400

    Use front encoders for feedback
    rear left encoder is funky

--- motor_controller.X/encoder_feedback.c
+++ motor_controller.X/encoder_feedback.c
@@ -18,8 +18,8 @@ unsigned int interrupt7_count = 0;
 unsigned int interrupt8_count = 0;
 unsigned int interrupt1_period = 0;
 unsigned int interrupt2_period = 0;
-unsigned int interrupt7_period = 0;
-unsigned int interrupt8_period = 0;
+unsigned int interrupt3_period = 0;
+unsigned int interrupt4_period = 0;
 
 void configure_encoders(){
     //Configure encoder feedback pins for input
@@ -32,21 +32,22 @@ void configure_encoders(){
     T3CONbits.TON = 1;
     //OpenTimer3(T3_ON & T3_GATE_OFF & T3_PS_1_8 & T3_SOURCE_INT, 0xFF);
     OpenCapture1(IC_ENCODER_CONFIG);
-    //OpenCapture2(IC_ENCODER_CONFIG);
+    OpenCapture2(IC_ENCODER_CONFIG);
     //OpenCapture7(IC_ENCODER_CONFIG);
-    OpenCapture8(IC_ENCODER_CONFIG);
+    //OpenCapture8(IC_ENCODER_CONFIG);
 
     ConfigIntCapture1(IC_ENCODER_INTERRUPT);
-    //ConfigIntCapture2(IC_ENCODER_INTERRUPT);
+    ConfigIntCapture2(IC_ENCODER_INTERRUPT);
     //ConfigIntCapture7(IC_ENCODER_INTERRUPT);
-    ConfigIntCapture8(IC_ENCODER_INTERRUPT);
+    //ConfigIntCapture8(IC_ENCODER_INTERRUPT);
 }
 
 //FR interrupt
 void __attribute__((__interrupt__, no_auto_psv)) _IC1Interrupt(void){
     ReadCapture1(&interrupt1_times[0]);
+    interrupt1_count++;
     interrupt1_period = interrupt1_times[0] - interrupt1_times[1];
-    right_drive_tick(1, interrupt1_period, interrupt8_period);
+    right_drive_tick(1, interrupt1_period, interrupt2_period);
     IFS0bits.IC1IF = 0;
 }
 
@@ -59,24 +60,24 @@ unsigned int right_period(){
 }
 
 //FL interrupt
-//IC1
+//IC2
+void __attribute__((__interrupt__, no_auto_psv)) _IC2Interrupt(void){
+    ReadCapture2(&interrupt2_times[0]);
+    interrupt2_count++;
+    interrupt2_period = interrupt2_times[0] - interrupt2_times[1];
+    left_drive_tick(1, interrupt2_period, interrupt1_period);
+    IFS0bits.IC2IF = 0;
+}
+
+unsigned int left_encoder_count(){
+    return interrupt2_count;
+}
+
+unsigned int left_period(){
+    return interrupt2_period;
+}
 
 //RR
 //IC7
 
 //RL read interrupt
-void __attribute__((__interrupt__, no_auto_psv)) _IC8Interrupt(void){
-    ReadCapture8(&interrupt8_times[0]);
-    interrupt8_count++;
-    interrupt8_period = interrupt8_times[0] - interrupt8_times[1];
-    left_drive_tick(1, interrupt8_period, interrupt1_period);
-    IFS1bits.IC8IF = 0;
-}
-
-unsigned int left_encoder_count(){
-    return interrupt8_count;
-}
-
-unsigned int left_period(){
-    return interrupt8_period;
-}
